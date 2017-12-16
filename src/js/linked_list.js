2577425d4a049649e4ca81e758b515bd7581db75
jensjoh01 <johnnyj2439@gmail.com>
2017-12-15 20:08:21 -0800

    completed linked list with tests in JS

--- src/js/linked_list.js
+++ src/js/linked_list.js
@@ -2,36 +2,107 @@
 
 class Node {
     constructor(val) {
-        this.val = val;
-        this.next = null;
+        this.val = val
+        this.next = null
     }
 }
 
 class LinkedList {
-    constructor() {
-        this.head = null;
-        this.size = 0
+    constructor(iterable = []) {
+        this.head = null
+        this.length = 0
+        for (var i = 0; i < iterable.length; i++) {
+            this.push(iterable[i]);
+        }
     }
 
     push(val) {
         var new_node = new Node(val)
-        if(this.size === 0) {
+        if(this.length === 0) {
             this.head = new_node
-            this.size++
+            this.length++
         } else {
             const curr = this.head
             new_node.next = curr
             this.head = new_node
-            this.size++
+            this.length++
         }
     }
 
     pop() {
-        if(this.size === 0) {
+        if(this.length === 0) {
             throw('Your LinkedList is empty.')
         } else {
-            
+            const toReturn = this.head
+            this.head = toReturn.next
+            this.length--
+            return toReturn.val
         }
+    }
+
+    size() {
+        return this.length
+    }
+
+    search(val) {
+        if(this.length === 0) {
+            throw('Your LinkedList is empty.')
+        } else {
+            let curr = this.head
+            let found = false
+            while(!found) {
+                if(curr.val === val) {
+                    found = true
+                } else if(curr.next) {
+                    curr = curr.next
+                } else {
+                    return null
+                }
+            }
+            return curr
+        }
+    }
+
+    remove(val) {
+        if(this.length === 0) {
+            throw('Your LinkedList is empty.')
+        } else {
+            let curr = this.head
+            let found = false
+            if(curr.val === val) {
+                let toRemove = curr
+                this.head = curr.next
+                curr.next = null
+                this.length--
+                return
+            }
+            let prev = null
+            while(!found) {
+                if(curr.val === val) {
+                    found = true
+                    let toRemove = curr
+                    prev.next = curr.next
+                    curr.next = null
+                    this.length--
+                    return
+                } else if(curr.next) {
+                    prev = curr
+                    curr = curr.next
+                } else {
+                    throw('Val not in LinkedList.')
+                }
+            }
+        }       
+    }
+
+    display() {
+        let toDisplay = []
+        let curr = this.head
+        while(curr) {
+            toDisplay.push(curr.val)
+            curr = curr.next
+        }
+        return toDisplay.reverse()
     }
 }
 
