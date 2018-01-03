788ff0ce86e0b6eb9b27faa3c2dd3fabdb35c689
jensjoh01 <johnnyj2439@gmail.com>
2018-01-02 18:18:13 -0800

    radix sort

---
+++ src/radix_sort.py
@@ -0,0 +1,56 @@
+"""Radix sort module."""
+
+
+def radix_sort(numbers):
+    """."""
+    sorting_list = []
+    final_list = []
+    buckets = {
+        0: [],
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+        5: [],
+        6: [],
+        7: [],
+        8: [],
+        9: []
+    }
+    for number in numbers:
+        sorting_list.append(str(number)[::-1])
+    sorting = True
+    i = 0
+    while sorting:
+        sorting = False
+        for str_num in sorting_list:
+            try:
+                buckets[int(str_num[i])].append(str_num)
+                sorting = True
+            except IndexError:
+                buckets[0].append(str_num)
+        sorting_list = []
+        for bucket_key in range(10):
+            for number_in_bucket in buckets[bucket_key]:
+                sorting_list.append(number_in_bucket)
+        buckets = {
+            0: [],
+            1: [],
+            2: [],
+            3: [],
+            4: [],
+            5: [],
+            6: [],
+            7: [],
+            8: [],
+            9: []
+        }
+        i += 1
+
+    for str_rev_num in sorting_list:
+        final_list.append(int(str_rev_num[::-1]))
+    print(final_list)
+
+
+if __name__ == '__main__':
+    radix_sort([100, 10, 99, 5, 999999, 52])
