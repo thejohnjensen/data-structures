e8dc61535eff657e48bd4d1e5bd7313b379bc3be
jensjoh01 <johnnyj2439@gmail.com>
2018-02-02 11:09:03 -0800

    dfs

--- src/graph_b.py
+++ src/graph_b.py
@@ -1,6 +1,6 @@
 """Graph created with seperate classes for Node and Graph."""
 from collections import defaultdict
-import Queue
+import queue
 
 
 class Graph(object):
@@ -8,7 +8,7 @@ class Graph(object):
 
     def __init__(self):
         """Init with defualt dict type set."""
-        self.graph = defaultdict(set)
+        self.graph = defaultdict(list)
 
     def add_node(self, data):
         """Add the node to the graph and create set."""
@@ -16,22 +16,28 @@ class Graph(object):
 
     def add_edge(self, start_node, end_node):
         """Add edge, if end node not in graph add it."""
-        self.graph[start_node].add(end_node)
+        self.graph[start_node].append(end_node)
 
-    def depth_first_search(self):
+    def dfs_recur_1(self, start, visited=[]):
         """."""
-        pass
+        visited += [start]
+
+        for i in self.graph[start]:
+            if i not in visited:
+                self.dfs_recur_1(i, visited)
+
+        return visited
 
     def breadth_first_search(self, start):
         """."""
-        que = Queue.Queue()
-        visited = []
+        que = queue.Queue()
+        visited = [start]
         que.put(start)
         while not que.empty():
             node = que.get()
-            visited.append(node)
             for i in self.graph[node]:
                 if i not in visited:
+                    visited.append(i)
                     que.put(i)
         return visited
 
@@ -43,8 +49,16 @@ class Graph(object):
         """."""
 
 
-
-
+if __name__ == '__main__':
+    g = Graph()
+    g.add_edge('A', 'B')
+    g.add_edge('A', 'C')
+    g.add_edge('B', 'C')
+    g.add_edge('B', 'D')
+    g.add_edge('C', 'D')
+    g.add_edge('D', 'C')
+    g.add_edge('E', 'F')
+    g.add_edge('F', 'C')
 
 
 
